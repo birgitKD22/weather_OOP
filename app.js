501028de915f4782547618112066e17b65262215
birgitKD22 <114754287+birgitKD22@users.noreply.github.com>
2023-03-12 15:51:47 +0200

    Update app.js task 4

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const weather = new Weather(weatherLocation)
 const ui = new UI()
 
+const form = document.querySelector('#change-city')
+form.addEventListener('submit', changeCityWeather)
+
+function changeCityWeather(event){
+	const city = document.querySelector('#city-name').value
+   weather.changeCity(city)
+	getWeather()
+	document.querySelector('#city-name').value = ''
+	event.preventDefault()
+
 // get city weather
 function getWeather() {
    weather.getWeather()
