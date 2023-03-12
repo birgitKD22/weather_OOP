080bf223523565245c473586838e75cb9d69c287
birgitKD22 <114754287+birgitKD22@users.noreply.github.com>
2023-03-12 15:41:13 +0200

    Weather.js updated

--- Weather.js
+++ Weather.js
@@ -1,10 +1,16 @@
 class Weather{
 	constructor(city){
-		this.key = '8c5970635a367c3807587c04fbc0f958';
+		this.key = '77f97b598769bf8861004c74e30ebbba';
 		this.city = city;
 	}
-async getWeather(){
-	const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
-	const responceData = await responce.json()
-	return responceData
+
+	async getWeather(){
+		const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+		const responceData = await responce.json()
+		return responceData
 	}
+
+	changeCity(city){
+		this.city = city;
+	}
+}
